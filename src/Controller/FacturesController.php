e372fe2ddd4bb74b8afb707437e8a62d5fe6483a
IronQuent <quentin.thomasson2@etu.univ-lyon1.fr>
2018-12-19 16:41:30 +0100

    [All]Create Files2

---
+++ src/Controller/FacturesController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p1701300
+ * Date: 19/12/2018
+ * Time: 15:08
+ */
+
+namespace App\Controller;
+
+
+class FacturesController extends AppController
+{
+
+}
